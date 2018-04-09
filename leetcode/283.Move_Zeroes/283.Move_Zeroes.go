89f4202a38d3414822354f692c2b359b9377645d
wangrui <wangrui@rongshu.tech>
2018-04-09 10:21:18 +0800

    finish 283 move zeroes

--- leetcode/283.Move_Zeroes/283.Move_Zeroes.go
+++ leetcode/283.Move_Zeroes/283.Move_Zeroes.go
@@ -5,6 +5,26 @@ func moveZeroes(nums []int) {
 		return
 	}
 
+	lastNonZeroFoundAt, cur := 0, 0
+	for ; cur < len(nums); cur++ {
+		if nums[cur] != 0 {
+			nums[lastNonZeroFoundAt] = nums[cur]
+			lastNonZeroFoundAt++
+		}
+	}
+
+	for zero := lastNonZeroFoundAt; zero < len(nums); zero++ {
+		nums[zero] = 0
+	}
+
+}
+
+//the better
+func moveZeroesII(nums []int) {
+	if len(nums) <= 1 {
+		return
+	}
+
 	for lastNonZeroFoundAt, cur := 0, 0; cur < len(nums); cur++ {
 		if nums[cur] != 0 {
 			nums[lastNonZeroFoundAt], nums[cur] = nums[cur], nums[lastNonZeroFoundAt]
--- leetcode/283.Move_Zeroes/283.Move_Zeroes_test.go
+++ leetcode/283.Move_Zeroes/283.Move_Zeroes_test.go
@@ -22,6 +22,25 @@ func TestMoveZeroes(t *testing.T) {
 	checkArray(ast, test3, []int{0, 0})
 }
 
+
+func TestMoveZeroesII(t *testing.T) {
+	ast := assert.New(t)
+
+	test1 := []int{0, 0, 1}
+	moveZeroesII(test1)
+	checkArray(ast, test1, []int{1, 0, 0})
+
+	test2 := []int{4, 2, 4, 0, 0, 3, 0, 5, 1, 0}
+	moveZeroesII(test2)
+	checkArray(ast, test2, []int{4, 2, 4, 3, 5, 1, 0, 0, 0, 0})
+
+	test3 := []int{0, 0}
+	moveZeroesII(test3)
+	checkArray(ast, test3, []int{0, 0})
+}
+
+
+
 func checkArray(ast *assert.Assertions, src, dest []int) {
 	ast.Equal(len(src), len(dest))
 
