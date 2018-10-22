425772e501501f9fdeab84f24cec4f31766e3649
vgmdj <228226928@qq.com>
2018-10-22 18:43:04 +0800

    add dp sulotion

--- leetcode/0042.Trapping_Rain_Water/0042.Trapping_Rain_Water.go
+++ leetcode/0042.Trapping_Rain_Water/0042.Trapping_Rain_Water.go
@@ -33,3 +33,41 @@ func trap(height []int) int {
 	return result
 
 }
+
+//dynamic programming
+func trap2(height []int) int {
+	if len(height) <= 2 {
+		return 0
+	}
+
+	result := 0
+	leftMax, rightMax := make([]int, len(height)), make([]int, len(height))
+
+	leftMax[0] = height[0]
+	for i := 1; i < len(height); i++ {
+		if height[i] > leftMax[i-1] {
+			leftMax[i] = height[i]
+		} else {
+			leftMax[i] = leftMax[i-1]
+		}
+	}
+
+	rightMax[len(height)-1] = height[len(height)-1]
+	for i := len(height) - 2; i >= 0; i-- {
+		if height[i] > rightMax[i+1] {
+			rightMax[i] = height[i]
+		} else {
+			rightMax[i] = rightMax[i+1]
+		}
+	}
+
+	for i := 0; i < len(height)-1; i++ {
+		if leftMax[i] < rightMax[i] {
+			result += leftMax[i] - height[i]
+		} else {
+			result += rightMax[i] - height[i]
+		}
+	}
+
+	return result
+}
--- leetcode/0042.Trapping_Rain_Water/0042.Trapping_Rain_Water_test.go
+++ leetcode/0042.Trapping_Rain_Water/0042.Trapping_Rain_Water_test.go
@@ -12,4 +12,6 @@ func TestTrap(t *testing.T) {
 	test1 := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
 	ast.Equal(6, trap(test1))
 
+	ast.Equal(6, trap2(test1))
+
 }
