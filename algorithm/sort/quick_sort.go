55776880e97d028d15a09099c1298ff7cea60b46
vgmdj <228226928@qq.com>
2019-02-27 09:53:36 +0800

    修正并添加注释

--- algorithm/sort/quick_sort.go
+++ algorithm/sort/quick_sort.go
@@ -1,24 +1,45 @@
 package sort
 
-//快速排序
+/*
+快速排序
+假设初始数组为
+5 4 3 2 1 8 9 0
+h n           t
+m
+
+如果和选定中间值比较,
+大于m, 与t上的值交换, 并使t前移
+小于等于m, 与上一位置上的值交换, 即与h上的值交换, 并将h与n后移
+(所以for循环条件可写成 head < tail 或者 next <= tail)
+h实际代表的是选定中间值的位置
+
+0 1 2 3 4 5 6 7 (p)
+4 3 2 1 0 5 9 8	(v)
+          h n
+          t
+所以, 5的位置已经排好, 还需要排 0-4 和 6-7的位置
+quickSort(values[:n-1])
+quickSort(values[n:])
+
+*/
 func quickSort(values []int) {
 	if len(values) <= 1 {
 		return
 	}
-	mid, i := values[0], 1
+	midValue, next := values[0], 1
 	head, tail := 0, len(values)-1
 	for head < tail {
-		if values[i] > mid {
-			values[i], values[tail] = values[tail], values[i]
+		if values[next] > midValue {
+			values[next], values[tail] = values[tail], values[next]
 			tail--
 		} else {
-			values[i], values[head] = values[head], values[i]
+			values[next], values[head] = values[head], values[next]
 			head++
-			i++
+			next++
 		}
 	}
 
-	quickSort(values[:i-1])
-	quickSort(values[i:])
+	quickSort(values[:next-1])
+	quickSort(values[next:])
 
 }
