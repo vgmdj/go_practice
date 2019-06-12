635594b98fd14854f4356393b7a12c6eb6d99484
vgmdj <228226928@qq.com>
2019-06-12 14:59:43 +0800

    optimized 4 median of two sorted arrays

--- leetcode/0004.Median_of_Two_Sorted_Arrays/4.Median_of_Two_Sorted_Arrays.go
+++ leetcode/0004.Median_of_Two_Sorted_Arrays/4.Median_of_Two_Sorted_Arrays.go
@@ -4,6 +4,75 @@ import (
 	"sort"
 )
 
+// O(log(m+n))
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	//ensure n >= m
+	m, n := len(nums1), len(nums2)
+	if m > n {
+		nums1, nums2 = nums2, nums1
+		m, n = n, m
+	}
+
+	iMin, iMax, halfLen := 0, m, (m+n+1)/2
+	for iMin <= iMax {
+		i := (iMin + iMax) / 2
+		j := halfLen - i
+		if i < iMax && nums2[j-1] > nums1[i] {
+			iMin = i + 1 // i is too small
+
+		} else if i > iMin && nums1[i-1] > nums2[j] {
+			iMax = i - 1 // i is too big
+
+		} else { // i is perfect
+			maxLeft := 0
+			if i == 0 {
+				maxLeft = nums2[j-1]
+
+			} else if j == 0 {
+				maxLeft = nums1[i-1]
+
+			} else {
+				maxLeft = Max(nums1[i-1], nums2[j-1])
+
+			}
+
+			if (m+n)%2 == 1 {
+				return float64(maxLeft)
+			}
+
+			minRight := 0
+			if i == m {
+				minRight = nums2[j]
+
+			} else if j == n {
+				minRight = nums1[i]
+
+			} else {
+				minRight = Min(nums2[j], nums1[i])
+			}
+
+			return float64(maxLeft+minRight) / 2.0
+		}
+	}
+	return 0.0
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
 // O((m+n)log(m+n))
 func findMedianSortedArrays1(nums1 []int, nums2 []int) float64 {
 
--- leetcode/0004.Median_of_Two_Sorted_Arrays/4.Median_of_Two_Sorted_Arrays_test.go
+++ leetcode/0004.Median_of_Two_Sorted_Arrays/4.Median_of_Two_Sorted_Arrays_test.go
@@ -3,8 +3,8 @@ package Median_of_Two_Sorted_Arrays
 import "testing"
 
 func TestOK(t *testing.T) {
-	nums1 := []int{1, 2}
-	nums2 := []int{3, 4}
+	nums1 := []int{1, 3, 5, 7, 9}
+	nums2 := []int{2, 3, 4, 5, 6, 7, 8, 9}
 
-	t.Log(findMedianSortedArrays2(nums1, nums2))
+	t.Log(findMedianSortedArrays(nums1, nums2))
 }
