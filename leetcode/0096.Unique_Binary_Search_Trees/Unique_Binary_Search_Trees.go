2a955ac72c92538439a6fab4d4731c0c4b39eb61
vgmdj <228226928@qq.com>
2019-06-12 09:46:11 +0800

    add  96 comment

--- leetcode/0096.Unique_Binary_Search_Trees/Unique_Binary_Search_Trees.go
+++ leetcode/0096.Unique_Binary_Search_Trees/Unique_Binary_Search_Trees.go
@@ -1,5 +1,16 @@
 package Unique_Binary_Search_Trees
 
+/*
+当n=3是 num = [1, 2, 3]
+此时根节点有三种情况：
+
+1为根节点时，1的左子树是0个，右子树是2,3（两个）。所以1是根节点的种类树是: res[0] * res[2]
+2为根节点时，2的左子树是1（1个），右子树是3（1个）。所以2是根节点的种类树是: res[1] * res[1]
+3为根节点时，3的左子树是1,2（2个），右子树是0个。所以3是根节点的种类树是: res[2] * res[0]
+所以 res[3] = res[0]*res[2] + res[1]*res[1] + res[2]*res[0]
+
+*/
+
 func numTrees(n int) int {
 	if n < 3 {
 		return n
