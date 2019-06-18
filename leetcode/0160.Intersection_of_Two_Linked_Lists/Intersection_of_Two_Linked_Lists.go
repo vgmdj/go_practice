ded74e035dc4f1a0d1181ae719df3eeda7c650cc
vgmdj <228226928@qq.com>
2019-06-18 11:51:04 +0800

    add a solution to 160 intersection of two linked lists

--- leetcode/0160.Intersection_of_Two_Linked_Lists/Intersection_of_Two_Linked_Lists.go
+++ leetcode/0160.Intersection_of_Two_Linked_Lists/Intersection_of_Two_Linked_Lists.go
@@ -6,6 +6,32 @@ type ListNode struct {
 }
 
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	curA := headA
+	curB := headB
+	for curA != nil && curB != nil {
+		curA = curA.Next
+		curB = curB.Next
+	}
+
+	for curA != nil {
+		headA = headA.Next
+		curA = curA.Next
+	}
+
+	for curB != nil {
+		headB = headB.Next
+		curB = curB.Next
+	}
+
+	for headA != headB {
+		headA = headA.Next
+		headB = headB.Next
+	}
+
+	return headA
+}
+
+func getIntersectionNode1(headA, headB *ListNode) *ListNode {
 	if headA == nil || headB == nil {
 		return nil
 	}
