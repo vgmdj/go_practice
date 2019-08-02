697de393d78ba854ff21f772fed5e5f1ba722cb7
vgmdj <228226928@qq.com>
2019-08-02 09:42:49 +0800

    add 222 count complete tree nodes

---
+++ leetcode/0222.Count_Complete_Tree_Nodes/Count_Complete_Tree_Nodes.go
@@ -0,0 +1,15 @@
+package Count_Complete_Tree_Nodes
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return countNodes(root.Left) + countNodes(root.Right) + 1
+}
