72e6009f1bc08d4453d0547ed78f01009241e9f1
wangrui <wangrui01@qingsongchou.com>
2020-01-21 09:38:10 +0800

    update backup

---
+++ leetcode/0095.Unique_Binary_Search_Trees_II/Unique_Binary_Search_Trees_II.go
@@ -0,0 +1,13 @@
+package Unique_Binary_Search_Trees_II
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func generateTrees(n int) []*TreeNode {
+	result := make([]*TreeNode, 0)
+
+	return result
+}
