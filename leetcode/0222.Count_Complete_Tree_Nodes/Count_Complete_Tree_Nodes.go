c6f895fdaf333583f9e8f2ce85848f1a6f257548
wangrui <228226928@qq.com>
2020-11-24 17:04:34 +0800

    optimize 222

--- leetcode/0222.Count_Complete_Tree_Nodes/Count_Complete_Tree_Nodes.go
+++ leetcode/0222.Count_Complete_Tree_Nodes/Count_Complete_Tree_Nodes.go
@@ -1,5 +1,7 @@
 package Count_Complete_Tree_Nodes
 
+import "sort"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -11,5 +13,29 @@ func countNodes(root *TreeNode) int {
 		return 0
 	}
 
-	return countNodes(root.Left) + countNodes(root.Right) + 1
+	// 根据最左叶子，算树的高度
+	level := 0
+	for node := root; node.Left != nil; node = node.Left {
+		level++
+	}
+
+	// 在最后一层中，利用二分搜索查找第一个空节点
+	// k的二进制位，代表从根节点到k节点的路径，
+	// 最高位代表是否是在第h层，其余各位从高到低，0代表要移动到左子树，1代表要移动到右子树
+	return sort.Search(1<<(level+1), func(k int) bool {
+		if k <= 1<<level {
+			return false
+		}
+		bits := 1 << (level - 1)
+		node := root
+		for node != nil && bits > 0 {
+			if bits&k == 0 {
+				node = node.Left
+			} else {
+				node = node.Right
+			}
+			bits >>= 1
+		}
+		return node == nil
+	}) - 1
 }
