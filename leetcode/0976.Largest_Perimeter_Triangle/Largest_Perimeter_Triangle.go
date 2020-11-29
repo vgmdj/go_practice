ddd5ad598a84c44aa2a69db5f44d14a684fc098e
wangrui <228226928@qq.com>
2020-11-29 12:08:20 +0800

    add: 976 largest perimeter triangle

---
+++ leetcode/0976.Largest_Perimeter_Triangle/Largest_Perimeter_Triangle.go
@@ -0,0 +1,24 @@
+package Largest_Perimeter_Triangle
+
+func largestPerimeter(A []int) int {
+	if len(A) < 3 {
+		return 0
+	}
+	for i := 0; i < len(A)-1; i++ {
+		for j := 0; j < len(A)-1-i; j++ {
+			if A[j] > A[j+1] {
+				A[j], A[j+1] = A[j+1], A[j]
+			}
+		}
+		if i >= 2 {
+			index := len(A) - 1 - i
+			if A[index]+A[index+1] > A[index+2] {
+				return A[index] + A[index+1] + A[index+2]
+			}
+		}
+	}
+	if A[0]+A[1] > A[2] {
+		return A[0] + A[1] + A[2]
+	}
+	return 0
+}
