26424421780e99e3adf58ddf1f8ddd0e06b68f3d
wangrui <228226928@qq.com>
2020-12-25 23:27:10 +0800

    add 455 assign cookies

---
+++ leetcode/0455.Assign_Cookies/Assign_Cookies.go
@@ -0,0 +1,18 @@
+func findContentChildren(g []int, s []int) int {
+	result := 0
+	sort.Ints(g)
+	sort.Ints(s)
+
+	index,c := 0,0
+	for  c < len(g) && index < len(s) {
+		if s[index] >= g[c] {
+			result++
+            c++
+        }
+
+        index++
+
+	}
+
+	return result
+}
