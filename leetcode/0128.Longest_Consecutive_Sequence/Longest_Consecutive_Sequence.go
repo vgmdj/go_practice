679b55374d73648c4e92181d5be357610860dfc0
wangrui <228226928@qq.com>
2020-12-29 09:45:33 +0800

    add 128 longest consecutive sequence

--- leetcode/0128.Longest_Consecutive_Sequence/Longest_Consecutive_Sequence.go
+++ leetcode/0128.Longest_Consecutive_Sequence/Longest_Consecutive_Sequence.go
@@ -1,1 +1,33 @@
 package Longest_Consecutive_Sequence
+
+func longestConsecutive(nums []int) int {
+	exist, result := make(map[int]bool, 0), 0
+	for _, c := range nums {
+		exist[c] = true
+	}
+
+	for _, c := range nums {
+		if !exist[c] {
+			continue
+		}
+
+		cLen := 1
+		for pre := c - 1; exist[pre]; pre-- {
+			cLen++
+			exist[pre] = false
+		}
+
+		for aft := c + 1; exist[aft]; aft++ {
+			cLen++
+			exist[aft] = false
+		}
+
+		if cLen > result {
+			result = cLen
+		}
+
+	}
+
+	return result
+
+}
